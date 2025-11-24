e66706716d98f6297e675a304f879fcf0685da53
pyang4071 <paddyyang4071@gmail.com>
2025-11-24 12:44:35 -0500

    added display variables

--- start_docker.sh
+++ start_docker.sh
@@ -1,6 +1,11 @@
+xhost +local:docker 
+# allows docker to use the x server 
+
 docker run -it --rm \
   -v $(pwd):/sim_ws \
   -w /sim_ws \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
   vehicle-dev
 
 
@@ -10,4 +15,5 @@ docker run -it --rm \
 # v flags 
 #   mounts the file/directory so edits on both docker and normal fs is synced
 # w - define the workspace
+# e export environment variables
 # name of docker image
